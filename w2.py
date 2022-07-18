69f1ed60dbda4900663310392586277d3b7cffdd
NIktoZi <92627120+NIktoZi@users.noreply.github.com>
2022-07-18 17:40:33 +0300

    Update w2.py

--- w2.py
+++ w2.py
@@ -1,9 +1,18 @@
-n=int(input())
-def fact(n):
-    f = 1
-    for i in range(n):
-        f *= i+1
-    return f
+# n=int(input())
+# def fact(n):
+#     f = 1
+#     for i in range(n):
+#         f *= i+1
+#     return f
  
-f = fact(n)
-print(f)
+# f = fact(n)
+# print(f)
+n=int(input('число'))
+su=0
+i=1
+while i<n:
+    su=(1+(1/i))**i
+    print(i,' ',su)
+    i+=1
+
+
